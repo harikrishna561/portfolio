34e9a483e1c5c1c132ca483f057439e77ad42460
Harikrishna Kalyanam <kharikrishna216@gmail.com>
2024-05-28 15:58:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/hari_porffolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hari_porffolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/main/components/drawer/personal_info.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../../res/constants.dart';
+import 'header_info.dart';
+
+class PersonalInfo extends StatelessWidget {
+  const PersonalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
+        AreaInfoText(title: 'Contact', text: '9701098826'),
+        AreaInfoText(title: 'Email', text: 'kharikrishna216@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: 'harikrishna-kalyanam-287722205'),
+        AreaInfoText(title: 'Github', text: 'haricmoon'),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
+        SizedBox(
+          height: defaultPadding,
+        ),
+      ],
+    );
+  }
+}
