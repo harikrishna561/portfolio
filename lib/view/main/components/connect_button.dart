5cf6e5a95f1e6c20703eac64f405a8df751ec50d
Harikrishna Kalyanam <kharikrishna216@gmail.com>
2026-05-28 10:31:22 +0530

    Update connect_button.dart

--- lib/view/main/components/connect_button.dart
+++ lib/view/main/components/connect_button.dart
@@ -12,7 +12,7 @@ class ConnectButton extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: defaultPadding),
       child: InkWell(
         onTap: () {
-          launchUrl(Uri.parse('https://wa.me/9701099826'));
+          launchUrl(Uri.parse('https://wa.me/9701098826'));
         },
         borderRadius: BorderRadius.circular(defaultPadding + 10),
         child: Container(
